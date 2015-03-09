3be6fc7522e43e2328bef2266b7ac0120b98b27d
Jaddori <packo_920@hotmail.com>
2015-03-09 14:03:03 +0100

    Adding linux main.

---
+++ linux_main.cpp
@@ -0,0 +1,20 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Tunder $
+   $Notice: (C) Copyright 2014 by SpaceCat, Inc. All Rights Reserved. $
+   ======================================================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "fairshare.cpp"
+#include "client.cpp"
+#include "server.cpp"
+
+int main( int argc, char** argv )
+{
+    return 0;
+}
